2de4e5dd82fe84b7ecafcd3618511e324e8388e7
zaidm124 <zaidm124@gmail.com>
2021-07-12 23:46:34 +0530

    Todo List

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import Header from "./MyComponents/Header"; /* When using rfc*/
+import { Footer } from "./MyComponents/Footer"; /* When using rafc*/
+import { Todos } from "./MyComponents/Todos"; /* When using rafc*/
+import React, { useState, useEffect } from "react";
+import { AddTodo } from "./MyComponents/AddTodo";
 
 function App() {
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const onDelete = (todo) => {
+    setTodos(
+      todos.filter((e) => {
+        return e !== todo;
+      })
+    );
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  const addTodo = (title, desc) => {
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    } else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    };
+    setTodos([...todos, myTodo]);
+  };
+
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="My Todos List" searchBar={true} />
+      <AddTodo addTodo={addTodo} />
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./footer.css";
+
+export const Footer = () => {
+  let footerStyle = {
+    // position: "absolute",
+    top: "87.5vh",
+    width: "100%",
+  };
+  return (
+    <footer
+      className="bg-dark text-light text-center my-3 py-3"
+      style={footerStyle}
+    >
+      copyright &copy; My todosList.com
+    </footer>
+  );
+};
